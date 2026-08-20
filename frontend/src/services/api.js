e94ca17087a92ffa7f18fd1bfc2cbe39fbe51f00
AdityaCodes15 <adityag152004@gmail.com>
2026-08-21 02:14:27 +0530

    Connect frontend to production backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: "https://skill-barter-mern.onrender.com/api"
 });
 
 export default api;
